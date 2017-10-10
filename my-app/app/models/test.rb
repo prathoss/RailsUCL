edc05b394c61550ea7192a64f2cb9880ee04118c
Unknown <m.bradac.michal@gmail.com>
2017-10-10 12:04:41 +0200

    Annotated Tests

--- my-app/app/models/test.rb
+++ my-app/app/models/test.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: tests
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  questions  :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Test < ApplicationRecord
 end
